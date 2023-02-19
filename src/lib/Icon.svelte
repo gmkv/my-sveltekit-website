42c42f7d93e462ee5bd1454babeac65d9f633295
gmkv <gmnullptr@gmail.com>
2023-02-19 13:40:53 +0200

    change font

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,13 @@
+<svg role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	<slot />
+</svg>
+
+<style>
+	svg {
+		fill: var(--primary);
+		height: 2rem;
+		width: 2rem;
+		margin-right: 0.5rem;
+		vertical-align: middle;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,17 @@
 	import "./styles.css";
 </script>
 
-<slot />
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: flex-start;
+		padding: clamp(3rem, calc(1.88rem + 5.63vw), 7.5rem);
+		min-height: 100dvh;
+	}
+</style>
